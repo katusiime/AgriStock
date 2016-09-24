803cedaee4f8ad51504fb172686e1ee610f89ee8
katusiime93 <katusiime93@yahoo.com>
2016-09-24 00:18:45 -0800

    my first commit

---
+++ customer.py
@@ -0,0 +1,15 @@
+class customer:
+	def _int_(self, name, days):
+		self.name = name
+		self.days = days
+
+class movie:
+	"""docstring for movie"""
+	def __init__(self, name, category = [], price):
+		
+		self.name = name
+		self.category = category
+		self.days = days
+		
+
+		
